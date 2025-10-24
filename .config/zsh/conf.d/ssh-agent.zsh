38078eadaaab294223cc93ade1436f1ededc5cf1
jKy0n <jk.1989@hotmail.com>
2025-10-24 15:50:28 -0300

    so many updates

--- .config/zsh/conf.d/ssh-agent.zsh
+++
@@ -1,7 +0,0 @@
-# ~/.config/zsh/conf.d/ssh-agent.zsh
-
-# Inicializa o ssh-agent
-eval "$(ssh-agent -s)" > /dev/null
-
-# Inicializa o keychain
-eval $(keychain --eval --quiet ~/.ssh/Crisnote-ssh-keys/github-crisnote)
